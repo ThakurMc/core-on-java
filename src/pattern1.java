bb6cf29b48b2d2212e5dfee68aa09c2b7b25eeb1
ThakurMc <mthakur16102000@gmail.com>
2023-02-25 13:36:31 +0530

    patternProgram1

--- src/pattern1.java
+++ src/pattern1.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class pattern1 {
+public class pattern1 {
+    public static void main(String[] args) {
+        for (int i=0;i<4;i++) {
+            for (int j=0; j<4; j++) {
+                System.out.print("*"+" ");
+            }
+            System.out.println(" ");
+        }
+    }
 }
